06365ce306a89add80800f03e14318b56711624c
Sahil Adhav <sahiladhav16@gmail.com>
2022-01-12 22:50:33 +0530

    Sahil Frontend added

---
+++ src/components/About_PregLoss.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styleAboutPeriods from './CSS/styleAboutPeriods.css'
+import Preg1 from "./images/Preg1.jpg";
+import Preg2 from "./images/Preg2.jpg";
+
+export default function About_PregLoss() {
+    return (
+        <div>
+            <section id="overview">
+                <h1 class="heading_big">Early and Recurrent Pregnancy Loss</h1>
+                <h3 class="heading_content">PCOS Pregnancy Loss</h3>
+                <p class="content">PCOS has numerous reproductive repercussions starting with anovulatory cycles leading to lesser chances of conceiving and even loss of fertility. Even after a successful conception, there are high chances of PCOS pregnancy loss, which is also known as the early loss of pregnancy (EPL). To add to it, if the woman with PCOS passes her third trimester, she may have to encounter later pregnancy complications like gestational diabetes mellitus (GDM).</p>
+
+<h3 class="heading_content">Why polycystic ovarian syndrome pregnancy lead to the Risk of Early Pregnancy Loss?</h3>
+<p class="content">Women with polycystic ovarian syndrome pregnancy symptoms are at risk of EPL. It is clinically tested that a first-trimester miscarriage can occur in 50% of pregnant women with PCOS, a rate which is three times higher than that in women without PCOS. Treatment of polycystic ovarian syndrome pregnancy with ovulation-induction agents is linked with higher rates of EPL compared to those in naturally conceiving women.
+
+</p>
+
+<img
+    src={Preg1}
+    class="img_overview"
+/>
+
+<h3 class="heading_content">Luteinizing Hormone and PCOS Pregnancy Loss at an early stage</h3>
+<p class="content">Several studies have linked elevated LH levels with EPL in women with PCOS. The chances of miscarriage increase when the level of LH is elevated, and the conception rate decreased in those with normal LH in PCOS women.</p>
+
+<h3 class="heading_content">Androgens and Early PCOS pregnancy loss</h3>
+<p class="content">Hyperandrogenemia is a conceivable cause of EPL in women PCOS. The presence of hyperandrogenemia or clinical hyperandrogenism is considered to be definitive for PCOS diagnosis. Elevated free/total testosterone ratios are tested to be predictive of EPL in women with PCOS, with the help of two different studies.</p>
+
+<h3 class="heading_content">Polycystic Ovarian Syndrome Recurrent Miscarriage</h3>
+<p class="content">Polycystic Ovarian Syndrome Recurrent Miscarriage or recurrent pregnancy loss (RPL), defined as two or more consecutive pregnancy losses before the 20th week of pregnancy, is a frequently encountered adverse event in patients suffering from this syndrome.
+
+It is important to remember there is no permanent cure for PCOS. The Polycystic Ovarian Syndrome pregnancy symptoms can only be controlled and managed on the bases of the treatment goals. For e.g., if you want to get pregnant, the doctor will regularly treat you with ovulation-inducing agents to improve your chances of getting pregnant and help you with a solution to Polycystic Ovarian Syndrome Recurrent Miscarriage
+
+</p>
+
+<img
+    src={Preg2}
+    class="img_overview"
+/>
+
+            </section>
+        </div>
+    )
+}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,133 @@
-import React from 'react'
+import React from "react";
+import styleFooter from "./CSS/styleFooter.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-    return (
-        <div>
-            <footer className="w-100 py-4 flex-shrink-0">
-      <div className="container py-4">
+  return (
+    <div>
+      <footer className="w-100 py-4 flex-shrink-0">
+        <div className="container py-4">
           <div className="row gy-4 gx-5">
-              <div className="col-lg-4 col-md-6">
-                  <h5 className="h1 text-black">PCOS <br/> COMPANION</h5>
-                  <p className="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
-                  <p className="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a  className="link footer-heading" className="text-primary" href="#">Pcoscompanion.com</a></p>
+            <div className="col-lg-4 col-md-6">
+              <h5 className="h1 text-black font-footer-heading">
+                <span className="spanTag">PCOS</span> <br /> COMPANION
+              </h5>
+              <p className="small text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+              <p className="small text-muted mb-0">
+                &copy; Copyrights. All rights reserved.{" "}
+                <a
+                  className="link footer-heading"
+                  className="text-primary"
+                  href="#"
+                >
+                  Pcoscompanion.com
+                </a>
+              </p>
+            </div>
+            <div className="col-lg-2 col-md-6">
+              <h5 className="text-black mb-3">Quick links</h5>
+              <ul className="foter-link list-unstyled text-muted">
+                <li>
+                  <a className="link" href="#">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="link" href="/about">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a className="link" href="/blog">
+                    Blogs
+                  </a>
+                </li>
+                <li>
+                  <a className="link" href="/diet">
+                    Diet
+                  </a>
+                </li>
+                <li>
+                  <a className="link" href="/tracker">
+                    Tracker
+                  </a>
+                </li>
+                <li>
+                  <a className="link" href="/tracker">
+                    About Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-6">
+              <h5 className="text-black mb-3">Follow Us</h5>
+              <div class="social-container">
+                <a
+                  href="https://www.youtube.com/"
+                  className="youtube social"
+                >
+                  <FontAwesomeIcon icon={faYoutube} size="2x" />
+                </a>
+                <br/>
+                <a
+                  href="https://www.facebook.com/"
+                  className="facebook social"
+                >
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <br/>
+                <a
+                  href="https://twitter.com/"
+                  className="twitter social"
+                >
+                  <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <br/>
+                <a
+                  href="https://www.instagram.com/"
+                  className="instagram social"
+                >
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
               </div>
-              <div className="col-lg-2 col-md-6">
-                  <h5 className="text-black mb-3">Quick links</h5>
-                  <ul className="foter-link list-unstyled text-muted">
-                      <li><a className="link" href="#">Home</a></li>
-                      <li><a className="link" href="#">About</a></li>
-                      <li><a className="link" href="#">Blogs</a></li>
-                      <li><a className="link" href="#">Diet</a></li>
-                      <li><a className="link" href="#">Tracker</a></li>
-                  </ul>
-              </div>
-              <div className="col-lg-2 col-md-6">
-                  <h5 className="text-black mb-3">Quick links</h5>
-                  <ul className="list-unstyled text-muted">
-                      <li><a className="link" href="#">Home</a></li>
-                      <li><a className="link" href="#">About</a></li>
-                      <li><a className="link" href="#">Blogs</a></li>
-                      <li><a className="link" href="#">Diet</a></li>
-                      <li><a className="link"  href="#">Tracker</a></li>
-                  </ul>
-              </div>
-              <div className="col-lg-4 col-md-6">
-                  <h5 className="text-black mb-3">Newsletter</h5>
-                  <p className="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
-                  <form action="#">
-                      <div className="input-group mb-3">
-                          <input className="form-control" type="text" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-                          <button className="btn btn-primary" id="button-addon2" type="button"><i className="fas fa-paper-plane"></i></button>
-                      </div>
-                  </form>
-              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <h5 className="text-black mb-3">Newsletter</h5>
+              <p className="small text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+              <form action="#">
+                <div className="input-group mb-3">
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="button-addon2"
+                  />
+                  <button
+                    className="btn btn-primary"
+                    id="button-addon2"
+                    type="button"
+                  >
+                    <i className="fas fa-paper-plane"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-      </div>
-  </footer>
         </div>
-    )
+      </footer>
+    </div>
+  );
 }

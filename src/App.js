5f43b1fc82affc6bd3c13eed0672057d0aa0ef50
Bulkpool <aashu.ayush@gmail.com>
2022-01-15 07:56:51 +0530

    up to date

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
-<<<<<<< HEAD
-
-import Blog from './components/Blog';
-import Blogcontent from './components/Blogcontent';
-import Homepage from './components/Homepage';
-import Login from './components/Login';
-import About from './components/About';
-
-import Chatbot from 'react-chatbot-kit'
-import 'react-chatbot-kit/build/main.css'
-import MessageParser from './components/bot/MessageParser';
-import ActionProvider from './components/bot/ActionProvider';
-import config from './components/bot/config';
-
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Diet from './components/Diet';
-import NoteState from './context/notes/NoteState';
-import User from './components/User';
-import About_overview from './components/About_overview';
-import About_cosmetic from './components/About_cosmetic';
-import About_irregularPeriods from './components/About_irregularPeriods';
-import About_infertility from './components/About_infertility';
-import About_PregLoss from './components/About_PregLoss';
-import About_gesDiabeates from './components/About_gesDiabtetes';
-import About_chronicDiabeates from './components/About_chronicDiabeates';
-import About_nutrition from './components/About_nutrition';
-import About_Sm from './components/About_Sm'; 
-import Diet_veg from './components/Diet_veg';
-import Diet_nonveg from './components/Diet_nonveg';
-
-import './App.css';
-
-=======
 import "./App.css";
 import Blog from "./components/Blog";
 import Blogcontent from "./components/Blogcontent";
 import Homepage from "./components/Homepage";
 import Login from "./components/Login";
 import About from "./components/About";
->>>>>>> f0dacbc6eead354a5b0f128dbc8ebf3843c8e85c
 
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
@@ -119,12 +78,7 @@ function App() {
         bottom : '50px',
         boxShadow : 'rgba(100,100,111,0.2) 0px 7px 29px 0px',
       }}>
-<<<<<<< HEAD
-        
-      <Chatbot
-=======
       {/* <Chatbot
->>>>>>> f0dacbc6eead354a5b0f128dbc8ebf3843c8e85c
           config={config}
           messageParser={MessageParser}
           actionProvider={ActionProvider}
